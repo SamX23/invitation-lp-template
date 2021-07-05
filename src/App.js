900f24e3a8154c60dda0207c558e2b5f96aced6a
Sami <skalexsong@gmail.com>
2021-07-05 21:13:58 +0700

    Update : add more basic layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Nav from "./component/nav";
 import Container from "./component/container";
 import styled from "@emotion/styled";
+import Footer from "./component/footer";
 
 const HeroElement = styled.div``;
 
@@ -9,11 +10,26 @@ const App = () => {
   return (
     <div className="App">
       <Nav />
+
       <Container>
         <HeroElement>
           <h1>Bismillah</h1>
         </HeroElement>
       </Container>
+      <Container>
+        <h1>Profile</h1>
+      </Container>
+      <Container>
+        <h1>Description</h1>
+      </Container>
+      <Container>
+        <h1>Gallery</h1>
+      </Container>
+      <Container>
+        <h1>Maps</h1>
+      </Container>
+
+      <Footer />
     </div>
   );
 };
