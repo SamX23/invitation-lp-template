4026e1d603fc8f5f8fe8f49c8bf3b998b46be116
SamX23 <skalexsong@gmail.com>
2021-07-09 22:06:55 +0700

    Update : footer component

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,5 +1,14 @@
 const Footer = () => {
-  return <footer>FOOTER</footer>;
+  return (
+    <footer>
+      Truly developed with{" "}
+      <span role="img" aria-label="love">
+        ♥️
+      </span>{" "}
+      by <a href="https://samx23.github.io">Sami Kalammallah</a> ©{" "}
+      {new Date().getFullYear()}
+    </footer>
+  );
 };
 
 export default Footer;
